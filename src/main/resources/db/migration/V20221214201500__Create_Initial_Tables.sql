d5b463ecc39722269eab136cc7d6db1925bcf6e1
Chun Lok Ling <chunlok.ling@digitalasset.com>
2022-12-16 16:48:11 +0000

    add json column

--- src/main/resources/db/migration/V20221214201500__Create_Initial_Tables.sql
+++ src/main/resources/db/migration/V20221214201500__Create_Initial_Tables.sql
@@ -4,5 +4,6 @@ create table events
     contract_id varchar not null,
     event_id    varchar not null,
     currency    varchar not null,
-    amount      numeric not null
+    amount      numeric not null,
+    json_data   json
 );
